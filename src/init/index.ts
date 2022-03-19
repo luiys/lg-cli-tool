ae749c57f69ff9d39784ab6995bb4482b53abb45
Luis Gustavo <Luisgustavofeitoza@gmail.com>
2022-03-19 04:16:26 -0300

    Editando .env e index.ts com dados do banco de dados

--- src/init/index.ts
+++ src/init/index.ts
@@ -2,7 +2,6 @@ import { createInterface } from 'readline';
 import shell from 'shelljs';
 import fs from 'fs'
 import util from 'util';
-import { bdConn } from './exclude';
 
 const readline = createInterface({
 	input: process.stdin,
@@ -16,11 +15,23 @@ export default async function init() {
 
 		const repoUri = 'https://github.com/luiys/nodejs-express-typeorm-boilerplate.git'
 		const options = ['y', 'n'];
+		const bdOptions = { flagBdType: 'postgres', flagBdName: 'db_name', flagBdUser: 'db_user', flagBdPassword: 'db_password', flagBdHost: 'db_host', flagBdPort: 'db_port' };
 
 		const projectName = await question('Nome do Projeto (new-node-project): ') ?? 'new-node-project'
 
 		const flagBdConnection = await question('Criar conexão com banco de dados? (y/n): ') ?? 'y'
 		if (!options.includes(flagBdConnection)) throw new Error('BD Opção inválida')
+
+		if (flagBdConnection === 'y') {
+			bdOptions.flagBdType = await question('Tipo de banco de dados (mysql/postgres/mssql): ') ?? 'postgres'
+			if (!['mysql', 'postgres', 'mssql'].includes(bdOptions.flagBdType.trim())) throw new Error('BD_TYPE Opção inválida')
+
+			bdOptions.flagBdName = await question('Nome do bd: ') ?? 'db_name'
+			bdOptions.flagBdUser = await question('User do bd: ') ?? 'db_user'
+			bdOptions.flagBdHost = await question('Host do bd: ') ?? 'db_host'
+			bdOptions.flagBdPassword = await question('Senha do bd: ') ?? 'db_password'
+			bdOptions.flagBdPort = await question('Porta do bd: ') ?? 'db_port'
+		}
 
 		const flagSendEmail = await question('O projeto vai enviar emails? (y/n): ') ?? 'y'
 		if (!options.includes(flagBdConnection)) throw new Error('EMAIL Opção inválida')
@@ -34,15 +45,38 @@ export default async function init() {
 		shell.cd(projectName)
 		if (!!shell.error()) throw new Error("Erro ao entrar no diretório");
 
-		// shell.exec('yarn')
-		// if (!!shell.error()) shell.exec('npm install')
-		// if (!!shell.error()) throw new Error("Erro ao instalar dependências");
+		shell.exec('yarn')
+		if (!!shell.error()) shell.exec('npm install')
+		if (!!shell.error()) throw new Error("Erro ao instalar dependências");
 
 		let env = fs.readFileSync('readme.md', 'utf8')
 		env = env.split('-----BEGIN .ENV-----')[1]
 		env = env.split('-----END .ENV-----')[0]
 		fs.writeFileSync('.env', env)
-		if (!!shell.error()) throw new Error("Erro ao criar arquivo .env");
+
+		if (flagBdConnection === 'y') {
+
+			let env = fs.readFileSync('.env', 'utf8')
+			env = env.replace('DB_HOST=db_host\r\n', `DB_HOST=${bdOptions.flagBdHost}\r\n`)
+			env = env.replace('DB_USER=db_user\r\n', `DB_USER=${bdOptions.flagBdUser}\r\n`)
+			env = env.replace('DB_NAME=db_name\r\n', `DB_NAME=${bdOptions.flagBdName}\r\n`)
+			env = env.replace('DB_HOST=db_host\r\n', `DB_HOST=${bdOptions.flagBdHost}\r\n`)
+			env = env.replace('DB_PASSWORD=db_password\r\n', `DB_PASSWORD=${bdOptions.flagBdPassword}\r\n`)
+			env = env.replace('DB_PORT=3306\r\n', `DB_PORT=${bdOptions.flagBdPort}\r\n`)
+			fs.writeFileSync('.env', env)
+
+			bdOptions.flagBdType === 'postgres' ? shell.exec('yarn add pg') : shell.exec(`yarn remove pg && yarn add ${bdOptions.flagBdType}`)
+			if (!!shell.error()) throw new Error("Erro ao instalar dependências do banco de dados");
+
+			const index = 'src/index.ts'
+			let content = fs.readFileSync(index, 'utf8')
+
+			content = content.replace('type: "postgres",', `type: "${bdOptions.flagBdType}",`)
+			fs.writeFileSync(index, content, 'utf8')
+
+		}
+
+
 
 		if (flagBdConnection === 'n') {
 
